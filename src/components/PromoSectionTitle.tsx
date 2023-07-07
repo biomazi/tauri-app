a935783d4fdd68a3dabf3160500ad64af92442ba
biomazi <biomazi@gmail.com>
2023-07-07 13:25:27 +0200

    release changes

--- src/components/PromoSectionTitle.tsx
+++ src/components/PromoSectionTitle.tsx
@@ -1,10 +1,11 @@
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import { FC } from "react";
 import CLSImage from "./CLSImage";
 import imgSrc from "../assets/invenda_logo.png";
 import LanguageSelect from "./LanguageSelect";
 
 const PromoSectionTitle: FC<{ title: string }> = ({ title }) => {
+  const handleClick = () => {};
   return (
     <Box
       sx={{
@@ -22,8 +23,12 @@ const PromoSectionTitle: FC<{ title: string }> = ({ title }) => {
         maxWidth={220}
         maxHeight={40}
       />
-      <Box display="flex" alignItems="center">
-        <Divider
+      <Box display="flex" alignItems="center" gap={2}>
+        <Button variant="contained" onClick={handleClick}>
+          Check for Update
+        </Button>
+        <Typography>Installing update</Typography>
+        {/* <Divider
           sx={{
             width: "80px",
             height: "6px",
@@ -55,7 +60,7 @@ const PromoSectionTitle: FC<{ title: string }> = ({ title }) => {
             background:
               "linear-gradient(270deg, #5E657E 0%, rgba(94, 101, 126, 0) 100%)",
           }}
-        />
+        /> */}
       </Box>
 
       <LanguageSelect />
