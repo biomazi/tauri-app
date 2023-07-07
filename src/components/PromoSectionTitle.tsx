29cac52ecde30f15fc821b477986e3ed3883b36b
biomazi <biomazi@gmail.com>
2023-07-07 13:38:25 +0200

    update 1

--- src/components/PromoSectionTitle.tsx
+++ src/components/PromoSectionTitle.tsx
@@ -27,7 +27,7 @@ const PromoSectionTitle: FC<{ title: string }> = ({ title }) => {
         <Button variant="contained" onClick={handleClick}>
           Check for Update
         </Button>
-        <Typography>Installing update</Typography>
+        <Typography>Installing update 1</Typography>
         {/* <Divider
           sx={{
             width: "80px",
